bf130ae578a97bab5c28fd82137c7fc05d95df0b
Rijon <rijon.r@yahoo.com>
2021-02-11 10:09:52 +0600

    final version

---
+++ src/containers/book_list.js
@@ -0,0 +1,51 @@
+//container have direct access to the redux
+import React, { Component } from 'react';
+import { connect} from 'react-redux';
+import { selectBook} from '../actions/index';
+import { bindActionCreators} from 'redux';//flows the state in different reducers
+
+export class BookList extends Component {
+
+    renderList(){
+        return this.props.books.map((book)=>{
+            return(
+                <li 
+                key={book.title}
+                onClick={()=> this.props.selectBook(book)}
+                className="list-group-item"
+                 >{book.title}</li>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <ul className="list-group col-sm-4">
+                {this.renderList()}
+            </ul>
+        )
+    }
+}
+function mapStateToProps(state){
+    //whatever is returned wil show up as props
+    //inside of Booklist
+    return{
+         books: state.books 
+    };
+}
+
+//Anything returened from this function will end up as props
+//on the booklist container
+function mapDispatchToProps(dispatch){
+    //take all the action and sent to diffrent reducer
+    //whenever selectBook is called , the result should be passed
+    //to all of our reducers
+    return bindActionCreators({
+        selectBook : selectBook
+    },dispatch)
+}
+
+//promote BookList from component to a container -it needs to know
+//about this new dispatch method,selectbook.Make it available
+//as a prop.
+export default connect(mapStateToProps , mapDispatchToProps )(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function (){
+    return [
+        { title : 'Javascript: The Good Parts' , pages : 101 },
+        { title :  'Harry Potter' , pages : 101 },
+        { title : 'The DARK TOWER' , pages : 11 },
+        { title : 'Rijon the reason' , pages : 10 }
+
+    ]
+}
